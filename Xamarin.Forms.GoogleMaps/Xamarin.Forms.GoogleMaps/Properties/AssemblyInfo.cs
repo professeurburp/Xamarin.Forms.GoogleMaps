42115a45fa4fb157360ee856202f1db2189b2645
Rémi Corbineau <remi.corbineau@gmail.com>
2017-09-15 14:40:02 +0200

    Project update
    - Move PCL to netstandard with VS 2017 new csproj format
    - Project reference one another instead of nuget platform packages
    - Update referenced nugets

--- Xamarin.Forms.GoogleMaps/Xamarin.Forms.GoogleMaps/Properties/AssemblyInfo.cs
+++ Xamarin.Forms.GoogleMaps/Xamarin.Forms.GoogleMaps/Properties/AssemblyInfo.cs
@@ -1,19 +1,9 @@
-using System.Reflection;
-using System.Resources;
 using System.Runtime.CompilerServices;
-using Xamarin.Forms;
-using Xamarin.Forms.Internals;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("Xamarin.Forms.GoogleMaps")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
 [assembly: InternalsVisibleTo("Xamarin.Forms.GoogleMaps.Android")]
 [assembly: InternalsVisibleTo("Xamarin.Forms.GoogleMaps.iOS")]
 [assembly: InternalsVisibleTo("Xamarin.Forms.GoogleMaps.UWP")]
-[assembly: Preserve]
